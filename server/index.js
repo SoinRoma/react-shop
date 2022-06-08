9a7ba3fd0f91be648b352af0735e807f1c6b619e
Soin Roman <soinroma26@gmail.com>
2022-06-08 10:34:17 +0500

    - add routers

--- server/index.js
+++ server/index.js
@@ -3,15 +3,13 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
+const router = require('./routes/index');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Working!'});
-});
+app.use('/api', router);
 
 const start = async () => {
     try {
