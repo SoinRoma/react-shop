e9304aa71927c340b0d7c206991609354682405f
Soin Roman <soinroma26@gmail.com>
2022-06-08 17:49:30 +0500

    - add function create for deviceController.js - add static folder for files

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,12 +1,26 @@
-class DeviceController {
-    async create(req, res) {
+const uuid = require('uuid');
+const path = require('path');
+const {Device} = require('../models/models');
+const apiError = require('../error/apiError');
 
+class DeviceController {
+    async create(req, res, next) {
+        try {
+            const {name, price, brandId, typeId, info} = req.body;
+            const {img} = req.files;
+            let fileName = uuid.v4() + '.png';
+            await img.mv(path.resolve(__dirname, '..', 'static', fileName));
+
+            const device = await Device.create({name, price, brandId, typeId, img: fileName});
+            return res.json(device);
+        } catch (e) {
+            next(apiError.badRequest(e.message));
+        }
     }
 
     async getAll(req, res) {
 
-    }
-
+    }f
     async getOne(req, res) {
 
     }
--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const models = require('./models/models');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const router = require('./routes/index');
+const path = require('path');
 const errorHandler = require('./middleware/errorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
 app.use('/api', router);
 
