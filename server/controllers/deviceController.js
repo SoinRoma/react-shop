f5c18a28cf20fad44a910d04724414e9d28876af
Soin Roman <soinroma26@gmail.com>
2022-06-09 10:12:15 +0500

    - add function getAll for deviceController

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -19,7 +19,24 @@ class DeviceController {
     }
 
     async getAll(req, res) {
-
+        let {brandId, typeId, limit, page} = req.query;
+        page = page || 1;
+        limit = limit || 10;
+        let offset = page *limit - limit;
+        let device;
+        if (!brandId && !typeId){
+            device = await Device.findAndCountAll({limit, offset});
+        }
+        if (brandId && !typeId){
+            device = await Device.findAndCountAll({where:{brandId}, limit, offset});
+        }
+        if (!brandId && typeId){
+            device = await Device.findAndCountAll({where:{typeId}, limit, offset});
+        }
+        if (brandId && typeId){
+            device = await Device.findAndCountAll({where:{brandId, typeId}, limit, offset});
+        }
+        return res.json(device);
     }
     async getOne(req, res) {
 
